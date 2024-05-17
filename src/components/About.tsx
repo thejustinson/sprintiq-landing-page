4d53f7f27d97737fb67d4bfe998aa4358edf7d2a
Nwachukwu Justin <63079391+thejustinson@users.noreply.github.com>
2024-05-17 16:08:35 +0100

    this designs are not easy to understadn

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,22 @@
 const About = () => {
   return (
-    <div className="">
+    <div className="px-10 py-10 lg:py-0">
         <div>
-            <h2 className="text-center text-[300px] font-bold text-[#1DAA4554]">
+            <h2 className="text-center text-7xl lg:text-[300px] font-bold text-[#1DAA4554]">
                 About
             </h2>
+            <div className="w-full flex justify-center">
+                <h3 className="bg-gradient-to-b from-[#E3F5E8] to-[#95D7A8] bg-clip-text text-transparent text-left flex justify-center lg:text-6xl font-bold max-w-[1000px] lg:mt-[-100px]">
+                    Incentivised Quiz Dapp for Community, Events, e.t.c
+                </h3>
+            </div>
+            <div className="lg:max-w-[1000px] text-lg mx-auto mt-10 flex flex-col gap-5">
+                <p>A web3 educational gaming platform with features that solidify knowledge in the blockchain through quiz test accompanied by rewards.</p>
+                <p>Rewards are earned in crypto and its value will be decided by the game creator.</p>
+            </div>
+        </div>
+        <div>
+
         </div>
     </div>
   )
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import Header from "./Header"
 
 const Home = () => {
   return (
-    <div className="radial-gradient">
+    <div className="radial-gradient relative">
       <Header/>
       <About/>
       Team
